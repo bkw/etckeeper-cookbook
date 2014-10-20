bb881bba8bcf7214f1de802ddc6daef77a144f3d
Bernhard K. Weisshuhn <bkw@codingforce.com>
2014-10-21 01:42:25 +0200

    bump chef spec log level to :warn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ require 'chefspec/cacher'
 RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version = '14.04'
-  config.log_level = :error
+  config.log_level = :warn
 end
 
 at_exit { ChefSpec::Coverage.report! }
