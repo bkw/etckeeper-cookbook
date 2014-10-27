ecdf951b1db01b05e69259f1fbaea5e298bc3e37
Bernhard K. Weisshuhn <bkw@codingforce.com>
2014-10-27 09:50:11 +0100

    simplify gem version check

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,25 +1,24 @@
 if defined?(ChefSpec)
-  if ChefSpec::VERSION
-    if Gem::Version.new(ChefSpec::VERSION) > Gem::Version.new('4.1.0')
-      ChefSpec.define_matcher(:etckeeper_git_remote)
-    else
-      ChefSpec::Runner.define_runner_method(:etckeeper_git_remote)
-    end
+  chefspec_version = Gem.loaded_specs['chefspec'].version
+  if chefspec_version < Gem::Version.new('4.1.0')
+    ChefSpec.define_matcher(:etckeeper_git_remote)
+  else
+    ChefSpec::Runner.define_runner_method(:etckeeper_git_remote)
+  end
 
-    def create_etckeeper_git_remote(resource)
-      ChefSpec::Matchers::ResourceMatcher.new(
-        :etckeeper_git_remote,
-        :create,
-        resource
-      )
-    end
+  def create_etckeeper_git_remote(resource)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :etckeeper_git_remote,
+      :create,
+      resource
+    )
+  end
 
-    def delete_etckeeper_git_remote(resource)
-      ChefSpec::Matchers::ResourceMatcher.new(
-        :etckeeper_git_remote,
-        :delete,
-        resource
-      )
-    end
+  def delete_etckeeper_git_remote(resource)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :etckeeper_git_remote,
+      :delete,
+      resource
+    )
   end
 end
