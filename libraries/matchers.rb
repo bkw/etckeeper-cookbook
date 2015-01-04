2991ef54ce084971906dcc6ba95d3ad4bffab5b0
Bernhard K. Weisshuhn <bkw@codingforce.com>
2015-01-04 22:44:02 +0100

    flip logic for chefspec matcher syntax

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,6 +1,6 @@
 if defined?(ChefSpec)
   chefspec_version = Gem.loaded_specs['chefspec'].version
-  if chefspec_version < Gem::Version.new('4.1.0')
+  if chefspec_version >= Gem::Version.new('4.1.0')
     ChefSpec.define_matcher(:etckeeper_git_remote)
   else
     ChefSpec::Runner.define_runner_method(:etckeeper_git_remote)
