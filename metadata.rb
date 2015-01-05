81d306ba5ceee01a90f80ef9135516a1b432df21
Bernhard K. Weisshuhn <bkw@codingforce.com>
2015-01-05 17:12:59 +0100

    bump to 2.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "bkw@codingforce.com"
 license          "Apache 2.0"
 description      "Installs/Configures etckeeper and remotes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.0"
+version          "2.0.1"
 replaces         "etckeeper"
 recipe           "etckeeper_git", "Install etckeeper"
 recipe           "etckeeper::enable", "configure and enable etckeeper"
